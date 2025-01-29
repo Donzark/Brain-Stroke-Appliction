b0d335293700ca2fdf391b9310bccad74b9697ce
Abdulrazak Adekunle <84377354+Donzark@users.noreply.github.com>
2025-01-29 14:34:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
-import os
-os.system("pip install tensorflow-hub")
+# import os
+# os.system("pip install tensorflow-hub")
+import sys
+sys.path.append("/home/appuser/.local/lib/python3.12/site-packages")
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
